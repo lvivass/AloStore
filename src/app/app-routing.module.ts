18875a7f10efbd2fb94ab42e4335de8fd4e52155
lvivass <lvivas@alodiga.com>
2023-12-18 08:06:40 -0400

    proyecto Alostore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //Pagina principal
+  { path: '', loadChildren: () => import('./pages/login/login.module').then((m) => m.LoginModule),},
+  
+  //Paginas secundarias **
+  { path: 'dashboard', loadChildren: () => import('./pages/dashboar/dashboard.module').then((m) => m.DashboardModule),},
+
+  { path: 'register', loadChildren: () => import('./pages/register/register.module').then((m) => m.RegisterModule),},
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  
 })
 export class AppRoutingModule { }
